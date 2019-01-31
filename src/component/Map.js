91ce4f3a889c3c3a227e56d3b2e6b59afd8fbf69
Dawn Corbin <imadesigner@hotmail.com>
2019-01-31 15:33:01 -0800

    Render Map component

--- src/component/Map.js
+++ src/component/Map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const MyMapComponent = withScriptjs(
-  withGoogleMap(props) => (
+  withGoogleMap(props => (
     <GoogleMap
       defaultZoom={8}
       defaultCenter={{ lat: -34.397, lng: 150.644 }}
