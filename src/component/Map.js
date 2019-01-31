0180c4f0857dc7d014d462bf9f72370614af2ad2
Dawn Corbin <imadesigner@hotmail.com>
2019-01-31 15:29:20 -0800

    Add API key

--- src/component/Map.js
+++ src/component/Map.js
@@ -19,7 +19,7 @@ export default class Map extends Component {
         return (
           <MyMapComponent
             isMarkerShown
-            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key"
+            googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAWa8VXVcDuIcPMIxrxjXMBRcpYta1zRnk"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `400px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
