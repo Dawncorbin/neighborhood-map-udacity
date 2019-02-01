9b550dd9cf8a64a4821c1512d44988b327810eba
Dawn Corbin <imadesigner@hotmail.com>
2019-02-01 15:02:19 -0800

    Turn keys object into Array

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,14 @@
 class Helper {
   static baseURL(){
-      
+      return "https://api.foursquare.com/v2"
+  }
+  static auth(){
+      const keys = {
+        client_id:"LYQJ5XBLTNYQF1UR2KYWSYPDBMVGCGTLUITEIK3G3112FUNR",
+        client_secret:"HJVFPNVFDAEFSSR4IPLCV0KLSWWEATXFVMR5LOOFYVPKXA0P",
+        v:"20190131"
+      };
+      return Object.keys(keys).map(key => `$[key]=${keys[key]}.join("&")`)
+
   }
 }
