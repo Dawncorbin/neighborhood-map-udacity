60181534c5b784840b864e47d220db11e37e98e0
Dawn Corbin <imadesigner@hotmail.com>
2019-02-05 13:56:50 -0800

    Add infor for each marker

--- src/App.js
+++ src/App.js
@@ -13,16 +13,19 @@ class App extends Component {
           zoom: 12
         };
     }
-    closeAllMarkers = () => {
-        const markers = this.state.markers(marker => {
-          marker.isOpen = false;
-          return marker;
-        });
-        this.setState({ markers: Object.assign(this.state.markers, markers) });
+  closeAllMarkers = () => {
+      const markers = this.state.markers.map(marker => {
+        marker.isOpen = false;
+        return marker;
+      });
+      this.setState({ markers: Object.assign(this.state.markers, markers) });
     };
   handleMarkerClick = (marker) => {
+      this.closeAllMarkers();
+      console.log(marker);
       marker.isOpen = true;
       this.setState({markers: Object.assign(this.state.markers,marker)})
+      SquareAPI.getVenueDetails(marker.id).then(res => console.log(res));
   }
   componentDidMount() {
       SquareAPI.search({
@@ -39,6 +42,7 @@ class App extends Component {
                 lng: venue.location.lng,
                 isOpen: false,
                 isVisible: true,
+                id:venue.id
               };
           });
           this.setState({ venues, center, markers });
