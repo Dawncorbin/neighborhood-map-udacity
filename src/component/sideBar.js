8dad0d06f4e405e71a03f4f428cd30bc68f67387
Dawn Corbin <imadesigner@hotmail.com>
2019-02-05 22:08:26 -0800

    Add font-size

--- src/component/sideBar.js
+++ src/component/sideBar.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import venueList from './venueList';
 
 export default class sideBar extends Component {
   render() {
       return (
         <div className="sideBar">
+          <input type={"search"} id={"search"} placeholder={"Filter Venues"} />
           <venueList/>
         </div>)
   }
