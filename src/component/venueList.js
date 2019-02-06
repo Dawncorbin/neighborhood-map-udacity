5255a3a879bb2487369472bb9fa03fa6024c2bb8
Dawn Corbin <imadesigner@hotmail.com>
2019-02-05 21:20:33 -0800

    Add venueList, move files to component folder

---
+++ src/component/venueList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class venueList extends Component {
+  render() {
+      return (
+        <ol className="venueList">
+
+        </ol>
+      )
+  }
+}
