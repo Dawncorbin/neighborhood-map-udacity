43152042d186ae2937487c1428eb0e5a67e36564
Dawn Corbin <imadesigner@hotmail.com>
2019-02-09 12:06:37 -0800

    Change input type

--- src/component/SideBar.js
+++ src/component/SideBar.js
@@ -48,7 +48,7 @@ export default class SideBar extends Component {
       return (
         <div className="SideBar">
           <input
-              input type={"Search"}
+              type={"Search"}
               id={"search"}
               placeholder={"Search Here"}
               onChange={this.handleChange}/>
