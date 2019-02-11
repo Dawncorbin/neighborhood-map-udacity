6b5c835fc50ac6c12dc8351bfbf8b13177da92a1
Dawn Corbin <imadesigner@hotmail.com>
2019-02-10 19:52:56 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -79,3 +79,74 @@ class App extends Component {
 }
 
 export default App;
+
+
+import React, { Component } from 'react';
+import './App.css';
+import Map from "./component/Map";
+import SideBar from "./component/SideBar";
+import SquareAPI from "./API/index";
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      venues:[],
+      center:[],
+      zoom: 12,
+      updateSuperState: obj =>{
+        this.setState(obj);
+      }
+    };
+  }
+  closeMarkers = () =>{
+    const markers = this.state.marker.map(marker =>{
+      marker.isOpen = false;
+      return marker;
+
+    });
+    this.setState({markers: Object.assign(this.state.markers, markers)})
+  }
+  markerClick = (marker) => {
+    this.closeMarkers();
+    marker.isOpen = true;
+    this.setState({markers: Object.assign(this.state.markers, marker)})
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+    SquareAPI.getDetails(marker.id).then(res =>{
+      const newVenue = Object.assign(venue, res.response.venue);
+      this.setState({venues: Object.assign(this.state.venues, newVenue)})
+      console.log(newVenue);
+    });
+  }
+  listItems = venue => {
+    const marker = this.state.markers.find(marker => marker.id === venue.id);
+    this.markerClick(marker);
+  }
+  componentDidMount(){
+    SquareAPI.search({near:"Seattle,WA", query:"pizza", limit:9}).then(result =>{
+      const { venues } = result.response;
+      const { center } = result.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat:venue.location.lat,
+          lng:venue.location.lng,
+          isOpen:false,
+          isVisible:true,
+          id:venue.id
+        };
+      });
+      this.setState({ venues, center, markers });
+      console.log(result);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+      <Menu width={"300"}>
+        <SideBar {...this.state} listItems={this.listItems} />
+      </Menu>
+        <Map aria-label="Map"{...this.state} MarkerClick = {this.markerClick}/>
+    );
+  }
+}
+export default App;
