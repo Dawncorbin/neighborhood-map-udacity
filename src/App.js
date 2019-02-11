46176cc8432956295a392f3dfe451bbb4bcf4349
Dawn Corbin <imadesigner@hotmail.com>
2019-02-10 19:58:18 -0800

    Made changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,3 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './component/Map';
-import SquareAPI from './API/';
-import SideBar from './component/SideBar';
-
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-          venues: [],
-          markers: [],
-          center: [],
-          zoom: 12,
-          updateSuperState: obj => {
-            this.setState(obj);
-          }
-        };
-    }
-
-  closeAllMarkers = () => {
-      const markers = this.state.markers.map(marker => {
-        marker.isOpen = false;
-        return marker;
-      });
-      this.setState({ markers: Object.assign(this.state.markers, markers) });
-    };
-//When marker is clicked, open info window
-  handleMarkerClick = (marker) => {
-      this.closeAllMarkers();
-      //console.log(marker);
-      marker.isOpen = true;
-      this.setState({markers: Object.assign(this.state.markers,marker)})
-
-      const venue = this.state.venues.find(venue => venue.id === marker.id);
-      SquareAPI.getVenueDetails(marker.id).then(res => {
-          const newVenue = Object.assign(venue,res.response.venue);
-          this.setState({ venues: Object.assign(this.state.venues, newVenue) });
-
-        });
-  };
-
-  handleMarkerClick = venue => {
-    const marker = this.state.markers.find(marker => marker.id === venue.id);
-    this.handleMarkerClick(marker);
-  };
-
-  componentDidMount(){
-      SquareAPI.search({
-          near:'Seattle, WA',
-          query: 'pizza',
-          limit: 9
-      }).then(results => {
-        console.log(results);
-          const { venues } = results.response.venues;
-          const { center } = results.response.geocode.feature.geometry;
-          const markers = results.response.venues.map(venue => {
-              return {
-               lat: venue.location.lat,
-               lng: venue.location.lng,
-               isOpen: false,
-               isVisible: true,
-               id: venue.id
-              };
-          });
-          this.setState({ venues, center, markers });
-      });
-  }
-
-  render() {
-    return (
-      <div className="App">
-          <SideBar {...this.state} handleListItemClick={this.handleListItemClick}/>
-        <Map aria-label="Map"{...this.state}
-        handleMarkerClick={this.handleMarkerClick}/>
-      </div>
-    );
-  }
-}
-
-export default App;
-
 
 import React, { Component } from 'react';
 import './App.css';
@@ -142,10 +60,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Menu width={"300"}>
+
         <SideBar {...this.state} listItems={this.listItems} />
-      </Menu>
+      
         <Map aria-label="Map"{...this.state} MarkerClick = {this.markerClick}/>
+      </div>
     );
   }
 }
