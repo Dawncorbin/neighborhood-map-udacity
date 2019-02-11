13b0a048685ce42061761958d5d7d7bb19f38a00
Dawn Corbin <imadesigner@hotmail.com>
2019-02-10 18:51:04 -0800

    Make changes

--- src/API/index.js
+++ src/API/index.js
@@ -1,70 +1,3 @@
-class Helper {
-  static baseURL(){
-      return "https://api.foursquare.com/v2"
-  }
-  static auth(){
-      const keys = {
-        client_id:"LYQJ5XBLTNYQF1UR2KYWSYPDBMVGCGTLUITEIK3G3112FUNR",
-        client_secret:"HJVFPNVFDAEFSSR4IPLCV0KLSWWEATXFVMR5LOOFYVPKXA0P",
-        v:"20190131"
-      };
-      return Object.keys(keys)
-          .map(key => `${key}=${keys[key]}`)
-          .join("&");
-  }
-  static urlBuilder(urlPrams){
-      if(!urlPrams){
-          return ""
-      }
-      return Object.keys(urlPrams)
-          .map(key => `${key}=${urlPrams[key]}`)
-          .join("&");
-  }
-  static headers() {
-      return {
-        Accept: "application/json"
-    };
-  }
-  //static verifyStatus(response) {
-  //  if (response.ok) {
-  //    return response;
-  //  } else {
-  //    let error = new Error(response.statusText);
-  //    error = response;
-  //    throw error;
-  //  }
-  //}
-
-  static simpleFetch(endPoint, method, urlPrams){
-      let requestData = {
-        method,
-        headers: Helper.headers()
-      };
-      return fetch(
-        `${Helper.baseURL()}${endPoint}${Helper.auth()}&${Helper.urlBuilder(
-        urlPrams
-      )}`,
-        requestData
-      )
-       .then(Helper.verifyStatus)
-       .then(res => res.json())
-        .catch(error => {
-            window.alert("ERROR! " + error);
-      });
-  }
-}
-export default class SquareAPI {
-    static search(urlPrams){
-      return Helper.simpleFetch(`/venues/search?`, "GET", urlPrams);
-    }
-    static getVenueDetails(VENUE_ID){
-      return Helper.simpleFetch(`/venues/${VENUE_ID}?`, "GET");
-    }
-    static getVenuePhotos(VENUE_ID) {
-      return Helper.simpleFetch(`/venues/${VENUE_ID}/photos?`, "GET");
-    }
-}
-
 
 class Helper{
     static baseUrl(){
@@ -72,8 +5,8 @@ class Helper{
     }
     static auth(){
       const keys = {
-        client id: "LYQJ5XBLTNYQF1UR2KYWSYPDBMVGCGTLUITEIK3G3112FUNR",
-        client secret: "HJVFPNVFDAEFSSR4IPLCV0KLSWWEATXFVMR5LOOFYVPKXA0P",
+        client_id: "LYQJ5XBLTNYQF1UR2KYWSYPDBMVGCGTLUITEIK3G3112FUNR",
+        client_secret: "HJVFPNVFDAEFSSR4IPLCV0KLSWWEATXFVMR5LOOFYVPKXA0P",
         v:"20190131"
       };
       return Object.keys(keys).map(key => `${key}=${keys[key]}`).join('&');
@@ -93,6 +26,7 @@ class Helper{
         error = response;
         throw error;
       }
+    }
     static headers(){
       return{
         Accept: "application/json"
@@ -103,7 +37,7 @@ class Helper{
         method,
         headers: Helper.headers()
       };
-      returns fetch(`${Helper.baseUrl()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(URLprams)}`, requestData)
+      return fetch(`${Helper.baseUrl()}${endPoint}?${Helper.auth()}&${Helper.urlBuilder(URLprams)}`, requestData)
       .then(response => response.json())
       .catch(error => {
         alert('An error occurred while trying to run program');
