ab935603376078b679c18889fdcbc75c7de6f541
Dawn Corbin <imadesigner@hotmail.com>
2019-02-10 19:33:25 -0800

    testing

--- src/component/VenueList.js
+++ src/component/VenueList.js
@@ -13,3 +13,19 @@ export default class VenueList extends Component {
       );
   }
 }
+
+import React, {Component} from "react";
+import ListItem from "./ListItem";
+
+export default class VenueList extends Component{
+    render(){
+      return(
+        <ol className="venueList">
+          {this.props.venues && this.props.venues.map((venue, idx) => (
+              <ListItem key={idx} {...venue} listItems={this.props.listItems}/>
+
+          ))}
+        </ol>
+      )
+    }
+}
