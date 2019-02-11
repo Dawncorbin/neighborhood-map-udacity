4e1b4fbc51426cd2b4b4712a79382941f8754b60
Dawn Corbin <imadesigner@hotmail.com>
2019-02-10 17:55:47 -0800

    Fix issues

--- src/component/ListItem.js
+++ src/component/ListItem.js
@@ -1,20 +1,4 @@
-import React, {Component} from 'react';
 
-export default class ListItem extends Component {
-  render() {
-      return (
-        <div tabIndex="0" className="ListItem" onClick={() => this.props.handleListItemClick(this.props)}>
-
-          <img
-            src=
-              {this.props.categories[0].icon.prefix+"32"+
-               this.props.categores[0].icon.suffix}
-            alt={this.props.categories[0].name}/>
-        {this.props.name}
-      </div>
-    );
-  }
-}
 import React, {Component} from 'react';
 
 export default class ListItem extends Component {
